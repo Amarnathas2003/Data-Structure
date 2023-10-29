2d5ed1c002b5ea8e63f7db30bdf9cdf535cc02de
Amarnath AS <amarnathas2003@gmail.com>
2023-10-29 16:09:32 +0530

    Binary Search and Recursion

---
+++ BinarySearch/binarySearch.js
@@ -0,0 +1,28 @@
+function binarySearch(array, key) {
+    let start = 0;
+    let end = array.length - 1;
+
+    while (start <= end) {
+        let mid = (start + end) / 2;
+
+        if (array[mid] == key) {
+            console.log("Element Found:-")
+            return array[mid];
+        }
+
+        if (array[mid] < key) { 
+            //Here the element will be in right side means vale is greater than mid value
+            start = mid + 1;
+        } else {
+            //array[mid] > key -> element will be in the left side of the array so we have to search there
+            end = mid - 1;
+        }
+    }
+
+    console.log("Element Not Found")
+    return -1;
+}
+
+const array = [10, 20, 30, 46, 55, 65, 80]
+const result = binarySearch(array, 55);
+console.log(result)
---
+++ BinarySearch/factorialRecursion.js
@@ -0,0 +1,55 @@
+function getFactorial(number) {
+
+    if (number === 0) {
+        return 1;
+    }
+
+    return number * getFactorial(number - 1)
+}
+
+// console.log(getFactorial(5))
+
+function sumOfNumbers(n) {
+
+    if (n === 1) {
+        return 1;
+    }
+
+    return n + sumOfNumbers(n - 1);
+}
+
+// console.log(sumOfNumbers(10))
+
+function fibonacciSerious(a, b, n) {
+
+    if (n == 0) {
+        return;
+    }
+
+    const c = a + b;
+    console.log(c)
+    //Ivide values previous function callil a b n ayi enthano pass cheythath athanu edukkunnath
+    fibonacciSerious(b, c, n - 1);
+}
+
+// const a = 0;
+// const b = 1;
+// console.log(a)
+// console.log(b)
+// const n = 7;
+// fibonacciSerious(a, b, n - 2)
+
+function printInRang(start, end) {
+
+    if (end == start) {
+        return;
+    }
+
+    printInRang(start, end - 1);
+    console.log(end);
+
+}
+const start = 1;
+const end = 10;
+
+printInRang(start, end)
