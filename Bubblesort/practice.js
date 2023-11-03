6aec3df87687b773fc587056862ca9323acfcc04
Amarnath AS <amarnathas2003@gmail.com>
2023-11-03 22:29:30 +0530

    Sorting Algorithm

--- Bubblesort/practice.js
+++ Bubblesort/practice.js
@@ -1,7 +1,7 @@
 
 function bubbleSort(array) {
     for (let i = 0; i < array.length; i++) {
-        
+
         for (let j = 1; j < array.length - i; j++) {
 
             if (array[j] < array[j - 1]) {
@@ -9,12 +9,8 @@ function bubbleSort(array) {
                 array[j] = array[j - 1];
                 array[j - 1] = temp;
             }
-
         }
     }
     return array;
 }
 
-
-const array = [4, 6, 7, 9, 2];
-console.log(bubbleSort(array));
--- Insertionsort/insertionSort.js
+++ Insertionsort/insertionSort.js
@@ -1,25 +1,19 @@
 
 
 function insertionSort(array) {
-
     for (let i = 0; i < array.length - 1; i++) {
-
         for (let j = i + 1; j > 0; j--) {
-
             if (array[j] < array[j - 1]) {
                 let temp = array[j];
                 array[j] = array[j - 1];
                 array[j - 1] = temp;
-
             } else {
                 break;
             }
         }
     }
-     
     return array;
 }
-
 
 const array = [30, 3, 4, 5];
 console.log(insertionSort(array));
